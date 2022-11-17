6fabd6ba0b62c9f6f219adfbbc9abd88e6aed6b4
N V Roshni <roshstar1999@gmail.com>
2022-11-17 06:29:06 +0530

    Update Rectangel_Area.py
    self solved

--- Rectangel_Area.py
+++ Rectangel_Area.py
@@ -3,14 +3,15 @@ class Solution:
         
         totalArea=abs((ax1-ax2)*(ay1-ay2))+abs((bx1-bx2)*(by1-by2))
         
+		#checking if the two rectangeles are intersecting or not
+		#just check if the x or y cordinate of top right corner of each rectangle is smaller than or equal to  the bottom  left corner  of the other rectangle
+		#if yes , then that means they are not intersecting, just return the toal of individual rectangles
+			
         if bx1>=ax2 or bx2<=ax1 or by2<ay1 or ay2<by1:
             return totalArea
         
-        
+        #else, return the total area- the intersecting area 
+		
         return totalArea-abs((max(ax1,bx1)-min(ax2,bx2))*(max(ay1,by1)-min(ay2,by2)))
-        
-      
-        
-        
-        
-        
+		
+		# feel free to reach out if any doubts
